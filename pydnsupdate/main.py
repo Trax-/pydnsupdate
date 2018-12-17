1b004dbf02b43f78ad8be3d37d88b2c601769892
Trevor Obermann <tlo@ocsnet.com>
2018-12-17 11:17:43 -0500

    Cleaned up some redundant code
    Signed-off-by: Trevor Obermann <tlo@ocsnet.com>

--- pydnsupdate/main.py
+++ pydnsupdate/main.py
@@ -28,38 +28,25 @@ def main():
     router_address_list_ipv6 = snmpdata.get_ip6_addresses(snmp_session)
     router_address_list_ipv6.sort()
 
-    name = rows[0][0]
     router_id = rows[0][2]
 
     for idx in range(0, 2):
-        print(f"{name}'s listed IP: {saved_address_list_ipv4[idx]} assigned IP {router_address_list_ipv4[idx]}")
-        print(f"{name}'s listed IP: {saved_address_list_ipv6[idx]} assigned IP {router_address_list_ipv6[idx]}")
+        print(f"{router_name}'s listed IP: {saved_address_list_ipv4[idx]} assigned IP {router_address_list_ipv4[idx]}")
+        print(f"{router_name}'s listed IP: {saved_address_list_ipv6[idx]} assigned IP {router_address_list_ipv6[idx]}")
 
     for address4 in router_address_list_ipv4:
         if address4 not in saved_address_list_ipv4:
             aws53.update(db, router_address_list_ipv4, 'A')
-            homedns.update(db, name, router_address_list_ipv4)
+            homedns.update(db, router_name, router_address_list_ipv4)
             db.insert_new(router_id, router_address_list_ipv4)
 
     for address6 in router_address_list_ipv6:
         if address6 not in saved_address_list_ipv6:
             aws53.update(db, router_address_list_ipv6, 'AAAA')
-            homedns.update(db, name, router_address_list_ipv6, 'AAAA')
+            homedns.update(db, router_name, router_address_list_ipv6, 'AAAA')
             db.insert_new(router_id, router_address_list_ipv6, 'AAAA')
 
     db.close()
-
-
-def testroute():
-    name = 'adsl2'
-    new_address = '98.1.2.4'
-
-    fred = dbdata.DbData()
-
-    names = fred.get_names_to_update_aws(name, new_address)
-
-    for name in names:
-        print(name)
 
 
 if __name__ == '__main__':
