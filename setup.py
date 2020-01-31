9c8dd89d53b5c43ef8fb50ed39742fd49cc81c72
Trevor Obermann <tlo@ocsnet.com>
2020-01-31 14:12:45 -0500

    Update new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'Trevor Obermann'
 
 setup(
     name='pydnsupdate',
-    version='3.0',
+    version='3.1',
     description='Dynamic DNS updater program',
     url='tlo@ocelot:/srv/git/PyDNSUpdate.git',
     author='Trevor Obermann',
