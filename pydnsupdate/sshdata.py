39ba489e68c76071798bc8f0495a474dc79a3e94
Trevor Obermann <tlo@ocsnet.com>
2021-02-20 12:11:50 -0500

    Code cleanup

--- pydnsupdate/sshdata.py
+++ pydnsupdate/sshdata.py
@@ -1,14 +1,3 @@
-import paramiko
-
-
-def get_client(router):
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.get_host_keys()
-
-    ssh_client.connect(hostname=router, username='ocelot')
-
-    return ssh_client
 
 
 def get_all_addresses(client):
@@ -16,7 +5,3 @@ def get_all_addresses(client):
     result = stdout.read().decode().split('\n')
     del result[4]
     return result
-
-
-def close_client(client):
-    client.close()
